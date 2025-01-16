a8a19ffeba55650169645da2203228f3d13e905b
antoruizx <antoruizx@gmail.com>
2025-01-16 17:37:36 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 3000; // O el puerto que estés usando
 
 app.get("/", (req, res) => res.send("Aplicación funcionando"));
 
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
